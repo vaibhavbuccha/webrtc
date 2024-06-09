9a15c70cee8273ca9a96f4bcd3a21f5f3c3bfe39
Vaibhav Buccha <jainvaibhav415@gmail.com>
2024-06-09 11:37:02 +0530

    Create a lobby screen

--- frontend/src/screens/Lobby.jsx
+++ frontend/src/screens/Lobby.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 
 const LobbyScreen = () => {
+
+    const [email, setEmail] = useState('');
+    const [room, setRoom] = useState("")
+
+    const handleSubmitForm = useCallback((e) => {
+        e.preventDefault();
+        console.log({
+            email,room
+        })
+    },[ email,room])
+
   return (
-    <div>LobbyScreen</div>
+    <div>
+        <div>LobbyScreen</div>
+        <form onSubmit={handleSubmitForm} >
+            <div>
+                <label htmlFor='email' >
+                    Email
+                </label>
+                <input type='email' id='email' onChange={e => setEmail(e?.target?.value)} value={email}  />
+            </div>
+            <div>
+                <label htmlFor='room' >
+                    Room Code
+                </label>
+                <input type='text' id='room'  onChange={e => setRoom(e?.target?.value)} value={room}  />
+            </div>
+           <div>
+                <button>
+                    Join
+                </button>
+           </div>
+            {/*  join */}
+        </form>
+    </div>
   )
 }
 
