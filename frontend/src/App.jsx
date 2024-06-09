85ce2c8919728152eed5af955c8db3cce76d83a0
Vaibhav Buccha <jainvaibhav415@gmail.com>
2024-06-09 11:18:05 +0530

    Setup react router dom and create our first lobby screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Route, Routes} from 'react-router-dom'
+import LobbyScreen from './screens/Lobby'
 
 function App() {
 
   return (
-    <>
-      APP
-    </>
+    <Routes>
+        <Route  
+          path='/'
+          element={ <LobbyScreen /> }
+          exact
+        />
+    </Routes>
   )
 }
 
---
+++ frontend/src/screens/Lobby.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LobbyScreen = () => {
+  return (
+    <div>LobbyScreen</div>
+  )
+}
+
+export default LobbyScreen
