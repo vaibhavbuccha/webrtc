4c5db479470d5445d4655bb2d2b667c4ce43b757
Vaibhav Buccha <jainvaibhav415@gmail.com>
2024-06-09 11:45:42 +0530

    Create contect for socket management

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const { Server } = require('socket.io');
 
-const io = new Server(8000);
+const io = new Server(8000, {
+    cors: true
+});
 
 io.on('connection', socket => {
     console.log('Socket connected ', socket?.id)
