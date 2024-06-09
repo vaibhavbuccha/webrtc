d04202b8679397094b78e376ed728bb5971a2927
Vaibhav Buccha <jainvaibhav415@gmail.com>
2024-06-09 16:22:00 +0530

    Added Own stream

--- frontend/src/screens/Room.jsx
+++ frontend/src/screens/Room.jsx
@@ -1,15 +1,22 @@
 import React,{useCallback, useEffect, useState} from 'react'
 import { useSocket } from '../context/SocketProvider'
+import ReactPlayer from 'react-player';
 
 const RoomPage = () => {
 
     const socket = useSocket();
     const [remoteSocketId, setRemoteSocketId] = useState(null);
+    const [ myStream, setMyStream ]= useState(null)
 
     const handleUserJoined = useCallback((data) => {
         const { email, id} = data;
         setRemoteSocketId(id);
         console.log(`${email} joined the room`)
+    },[])
+
+    const handleCallUser = useCallback( async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true, preferCurrentTab: true, peerIdentity: true })
+        setMyStream(stream);
     },[])
 
     useEffect(() => {
@@ -30,8 +37,25 @@ const RoomPage = () => {
         }
 
         {
+            myStream ? 
+            <>
+                <h4>
+                    My Stream
+                </h4>
+                <ReactPlayer 
+                    height="300px"
+                    width="300px"
+                    playing={true}
+                    url={myStream}
+                    muted
+                />
+            </>
+            : ""
+        }
+
+        {
             remoteSocketId ? 
-            <button>
+            <button onClick={handleCallUser} >
                 Call
             </button>
             : ""
